7535c8560190b5e44b6fdc8036bed905a0f4531c
Christian Pettet <71297280+cpettet@users.noreply.github.com>
2022-11-18 14:38:39 -0500

    again

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,9 @@ function doCompile {
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$BROWSER" != "chrome" -o "$BVER" != "stable" ]; then
     echo "Skipping deploy; just doing a build."
+    echo "${TRAVIS_PULL_REQUEST}"
+    echo "${TRAVIS_BRANCH} =?= ${SOURCE_BRANCH}"
+    echo "${BROWSER}"
     doCompile
     exit 0
 fi
